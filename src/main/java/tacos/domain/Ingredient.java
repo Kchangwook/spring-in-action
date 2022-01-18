5d67397e432da0dd576d00f8e22e5b7155d41fd2
kchangwook <kchangwook@naver.com>
2022-01-19 00:23:37 +0900

    [1] 타코 도메인 생성하기

---
+++ src/main/java/tacos/domain/Ingredient.java
@@ -0,0 +1,20 @@
+package tacos.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Ingredient {
+	private String id;
+	private String name;
+	private Type type;
+
+	public enum Type {
+		WRAP,
+		PROTEIN,
+		VEGGIES,
+		CHEESE,
+		SAUCE
+	}
+}
