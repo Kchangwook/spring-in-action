11d426161f469d591b49b119631ada146a284d55
kchangwook <dbwlalss2!>
2022-02-23 00:01:56 +0900

    LDAP 인증 구현

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -20,13 +21,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-			.withUser("user1")
-			.password("{noop}password1")
-			.authorities("ROLE_USER")
+		auth.ldapAuthentication()
+			.userSearchBase("ou=people")
+			.userSearchFilter("(uid{0})")
+			.groupSearchBase("ou=groups")
+			.groupSearchFilter("member={0}")
+			.contextSource()
+			.root("dc=tacocloud,dc=com")
+			.ldif("classpath:users.ldif")
 			.and()
-			.withUser("user2")
-			.password("{noop}password2")
-			.authorities("ROLE_USER");
+			.passwordCompare()
+			.passwordEncoder(new BCryptPasswordEncoder())
+			.passwordAttribute("userPasscode");
 	}
 }
