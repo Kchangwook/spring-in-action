08a06362453e57e3750134c1c9e6d5ecbed60815
kchangwook <dbwlalss2!>
2022-04-01 23:34:52 +0900

    5.2 구성 속성 생성하기

--- src/main/java/tacos/controller/OrderController.java
+++ src/main/java/tacos/controller/OrderController.java
@@ -2,6 +2,9 @@ package tacos.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,8 +27,15 @@ import tacos.repository.OrderRepository;
 @RequiredArgsConstructor
 @RequestMapping("/orders")
 @SessionAttributes("order")
+@ConfigurationProperties(prefix="taco.orders")
 public class OrderController {
 	private final OrderRepository orderRepository;
+
+	private int pageSize = 20;
+
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
 
 	@GetMapping("/current")
 	public String orderForm(@AuthenticationPrincipal User user,
@@ -64,5 +74,12 @@ public class OrderController {
 		orderRepository.save(order);
 		sessionStatus.setComplete();
 		return "redirect:/";
+	}
+
+	@GetMapping
+	public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+		Pageable pageable = PageRequest.of(0, pageSize);
+		model.addAttribute("orders", orderRepository.findByUserOrderByPlacedAtDesc(user, pageable));
+		return "orderList";
 	}
 }
--- src/main/java/tacos/repository/OrderRepository.java
+++ src/main/java/tacos/repository/OrderRepository.java
@@ -1,8 +1,13 @@
 package tacos.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import tacos.domain.Order;
+import tacos.domain.User;
 
 public interface OrderRepository extends CrudRepository<Order, Long> {
+	List<Order> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
