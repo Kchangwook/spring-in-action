2b54e063c2f515fe51b448539308a3456cac6243
kchangwook <dbwlalss2!>
2022-04-21 22:41:12 +0900

    6.2 하이퍼미디어 사용하기 - embedded 관계 이름 짓기

--- src/main/java/tacos/api/TacoEntityModel.java
+++ src/main/java/tacos/api/TacoEntityModel.java
@@ -4,12 +4,14 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
 
 import lombok.Getter;
 import tacos.domain.Ingredient;
 import tacos.domain.Taco;
 
 @Getter
+@Relation(value = "taco", collectionRelation = "tacos")
 public class TacoEntityModel extends RepresentationModel<TacoEntityModel> {
 	private final String name;
 	private final LocalDate createdAt;
